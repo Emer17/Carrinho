00a15e4364518b688296609b2a5aff6c15493db8
Leonardo Deliyannis Constantin <constantin.leo@gmail.com>
2015-04-10 09:21:45 -0300

    created files Cliente and Frete

---
+++ Cliente.java
@@ -0,0 +1,12 @@
+class Cliente{
+	private String nome;
+	private int CPF;
+	
+	/* SETTERS */
+	public void setNome(String s){ this.nome = s; }
+	public void setCPF(int N){ this.CPF = N; }
+	
+	/* GETTERS */
+	public String getNome(){ return this.nome; }
+	public int getCPF(){ return this.CPF; }
+}
